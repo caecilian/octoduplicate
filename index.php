9a3929d2ced06273dd551f334d86cdc8a7e78d1b
caecilian <31619483+caecilian@users.noreply.github.com>
2017-12-08 13:36:48 +0000

    Updating template files

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+<head lang="en">
+    <title>Chagos Information Portal</title>
+    <link rel="stylesheet" href="style_octodup.css" />
+</head>
+
+<body>
+    <?php
+include "headertemplate.html";
+?>
+    <article>
+        <header class=pinkblock>
+            <img src="https://octophindigital.com/images/chip%20interactive%20map%20resources%20site%20case%20study.jpg" class="aspect-ratio-box-inside">
+            <div class="box">
+                <h1>Chagos Information Portal
+                </h1>
+                <p>A resources library and interactive map landing page for UK charity the Chagos Conservation Trust.</p>
+            </div>
+        </header>
+        <div>
+            <h3>The Objective</h3>
+            <p>UK charity the Chagos Conservation Trust (CCT) approached Octophin Digital to create a new online information portal for scientific resources, species databases, and an interactive element to engage the wider public.
+            </p>
+            <h3>The Approach</h3>
+            <p>At the heart of the Chagos Information Portal is a thoughtfully constructed information database which was developed using Expression Engine and built around six different resource types: Publications, Datasets, Videos, Images, Expeditions, and People. The information and data can be easily filtered and searched by conservation scientists on the site and then viewed or downloaded.</p>
+
+            <p>To enable the public to &#34;dive in&#34; to the world&#39;s largest marine protected area, the homepage boasts a full-width interactive map of the Chagos Archipelago. The map shows the location of the small group of islands in the Indian ocean, the majority of which are not viewable on other online maps as many of them are too small or semi-submerged. For the first time the map reveals this biodiversity hotspot in detail for users to zoom in and pan around using their mouse of touchscreens. Each of the islands has a marker point showing the type of information it contains, from invertebrates and birds and invertebrates to plants and coral. In the Explore panel the user can read an overview of the island, see images, watch videos, read about expeditions and explore 360 degree panoramas. Simple visual icons enable the user to filter content on the map by interest area or species.</p>
+
+            <p>The mapping technology is provided by Mapbox through which the style and design of the map itself is completely customised. The interactive map then feeds directly off the information and assets stored within the main Expression Engine site so that the CCT team can add and edit content all in the same place. The custom map uses scientific shape data provided by Hamylton S, Andr&#233;fou&#235;t S showing depth through colour and more information about specific specific coral reefs and land areas in hover tooltips. The site also hosts two new species databases - Birds of Chagos and Corals of Chagos, online encyclopedias of the biodiversity of the Chagos Archipelago. </p>
+
+            <p>The project is funded by the John Ellerman Foundation.
+            </p>
+            <h3>Client testimonial</h3>
+            <blockquote class=testimonial>
+
+                <p>&#34;The challenge - develop a scientific information portal that included an interactive map with nearly 1,000 documents, images and videos in a matter of months. Challenge accepted by Octophin Digital who not only produced an excellent platform to showcase the science from one of the world&#39;s most remote place but managed to do it with the professionalism, efficiency and enthusiasm you rarely come across. </p>
+
+                <p>&#34;Octophin Digital have produced a wonderful interactive website for CCT that takes users on a journey through one of the world&#39;s most remote place.</p>
+
+                <p>&#34;CCT has had the pleasure of working with the team at Octophin Digital and would recommend them to any charity looking for a professional, efficient and enthusiastic team for all your digital creative needs.&#34;</p>
+
+                <cite>&#45; Helen Pitman, Director - Chagos Conservation Trust</cite>
+            </blockquote>
+
+            <a class="biglink" href="http://chagosinformationportal.org/">Visit the Chagos Information Portal &#9655;</a>
+        </div>
+    </article>
+
+   <?php
+include "footertemplate.html";
+?>
+    
+</body>
+
+
+</html>
